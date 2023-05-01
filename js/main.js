be3fed0817ef9a24316c747643ee06bf868ffcef
JeChangPark <wpckddl1124@naver.com>
2023-05-01 18:25:12 +0900

    0501

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// 로딩이 완료되면 로딩 화면을 닫음
+// Close the loading screen when loading is complete
+window.addEventListener("load", function () {
+  var loadingScreen = document.getElementById("loading_screen");
+  setTimeout(function () {
+    loadingScreen.style.display = "none";
+  }, 000);
+
+  var text1 = document.getElementById("text1");
+  setTimeout(function () {
+    text1.style.display = "none";
+  }, 2000);
+
+  var text2 = document.getElementById("text2");
+  setTimeout(function () {
+    text2.style.display = "block";
+  }, 2000);
+});
