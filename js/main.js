b03657769ff4d1437dda749641ed256aaebc4dc1
unknown <parkdory80@gmail.com>
2023-05-03 17:08:21 +0900

    0503

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
 // 로딩이 완료되면 로딩 화면을 닫음
 // Close the loading screen when loading is complete
 window.addEventListener("load", function () {
-    var loadingScreen = document.getElementById("loading_screen");
-    setTimeout(function () {
-        loadingScreen.style.display = "none";
-    }, 5000);
+  var loadingScreen = document.getElementById("loading_screen");
+  setTimeout(function () {
+    loadingScreen.style.display = "none";
+  }, 4000);
 
-    var text1 = document.getElementById("text1");
-    setTimeout(function () {
-        text1.style.display = "none";
-    }, 2000);
+  var text1 = document.getElementById("text1");
+  setTimeout(function () {
+    text1.style.display = "none";
+  }, 2000);
 
-    var text2 = document.getElementById("text2");
-    setTimeout(function () {
-        text2.style.display = "block";
-    }, 2000);
-    var first = document.querySelector(".first");
-    first.style.opacity = 0;
-    first.style.transform = "translateY(50px)";
-    setTimeout(function () {
-        first.style.opacity = 1;
-        first.style.transform = "translateY(0)";
-        first.style.transition =
-            "opacity 1s ease-in-out, transform 1s ease-in-out";
-    }, 5600);
-    var second = document.querySelector(".second");
-    second.style.opacity = 0;
-    second.style.transform = "translateY(50px)";
-    setTimeout(function () {
-        second.style.opacity = 1;
-        second.style.transform = "translateY(0)";
-        second.style.transition =
-            "opacity 1s ease-in-out, transform 1s ease-in-out";
-    }, 6000);
+  var text2 = document.getElementById("text2");
+  setTimeout(function () {
+    text2.style.display = "block";
+  }, 2000);
+  var first = document.querySelector(".first");
+  first.style.opacity = 0;
+  first.style.transform = "translateY(50px)";
+  setTimeout(function () {
+    first.style.opacity = 1;
+    first.style.transform = "translateY(0)";
+    first.style.transition = "opacity 1s ease-in-out, transform 1s ease-in-out";
+  }, 4600);
+  var second = document.querySelector(".second");
+  second.style.opacity = 0;
+  second.style.transform = "translateY(50px)";
+  setTimeout(function () {
+    second.style.opacity = 1;
+    second.style.transform = "translateY(0)";
+    second.style.transition =
+      "opacity 1s ease-in-out, transform 1s ease-in-out";
+  }, 5000);
 });
